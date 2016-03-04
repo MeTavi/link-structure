3c95b64c7d05936d8ce57b2b9cd8236756091c23
Shane Martin <dev.sh@nemart.in>
2016-03-04 16:41:43 -0500

    Cleanup

--- assets/js/app.js
+++ assets/js/app.js
@@ -165,18 +165,11 @@ function updateDate() {
 }
 
 function tick() {
-  links.attr("x1", function (d) {
-      return d.source.x;
-    })
-    .attr("y1", function (d) {
-      return d.source.y;
-    })
-    .attr("x2", function (d) {
-      return d.target.x;
-    })
-    .attr("y2", function (d) {
-      return d.target.y;
-    });
+  links
+    .attr("x1", function (d) { return d.source.x; })
+    .attr("y1", function (d) { return d.source.y; })
+    .attr("x2", function (d) { return d.target.x; })
+    .attr("y2", function (d) { return d.target.y; });
 
   node.attr("transform", function (d) {
     return "translate(" + d.x + "," + d.y + ")";
