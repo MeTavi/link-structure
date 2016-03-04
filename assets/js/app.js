84db8d9c7df576308c780705d97fd1368a23939a
Shane Martin <dev.sh@nemart.in>
2016-03-04 16:17:35 -0500

    Update JS to use JSON data

--- assets/js/app.js
+++ assets/js/app.js
@@ -190,53 +190,37 @@ function dblclick(d) {
   d3.select(this).classed("fixed", d.fixed = false);
 }
 
-// import and process link data
-function loadLinks() {
-  $.isLoading({text: "Loading", position: "overlay"});
 
-  d3.csv("/data/links.csv", function (error, data) {
-    max_links = data[0].count;
-    all_links = data.slice();
-    all_links.forEach(function(l) {
-      var source_str = l.source;
-      var target_str = l.target;
-      l.source = node_by_name[source_str];
-      l.target = node_by_name[target_str];
-      node_to_links[source_str].push(l);
-      if (source_str != target_str) {
-        var l2 = JSON.parse(JSON.stringify(l));
-        l2.source = node_by_name[target_str];
-        l2.target = node_by_name[source_str];
-        l2.date = l.date;
-        node_to_links[target_str].push(l2);
-      }
-    });
+// display loader
+$.isLoading({text: "Loading", position: "overlay"});
 
-    updateGraph();
+// import and process node data, then get link data
+d3.json("/data/metadata.json", function (error, data) {
 
-    $.isLoading("hide");
-  });
-}
-
-
-// When page is ready...
-(function($, window) {
-  // display loader
-  $.isLoading({text: "Loading", position: "overlay"});
-
-  // import and process node data, then get link data
-  d3.csv("/data/nodes.csv", function (error, data) {
-
-    all_nodes = data.slice();
-    all_nodes.forEach(function (n) {
-      node_to_links[n.name] = [];
-      node_by_name[n.name] = n;
-    });
-    num_nodes = all_nodes.length;
-
-    loadLinks();
-
-    $.isLoading("hide");
+  // process nodes
+  all_nodes = data.nodes;
+  all_nodes.forEach(function (n) {
+    node_to_links[n.name] = [];
+    node_by_name[n.name] = n;
   });
 
-}).call(this, jQuery, window);
+  // process link data
+  all_links = data.links;
+  all_links.forEach(function(l) {
+    var source_str = l.source;
+    var target_str = l.target;
+    l.source = node_by_name[source_str];
+    l.target = node_by_name[target_str];
+    node_to_links[source_str].push(l);
+    if (source_str != target_str) {
+      var l2 = l;
+      l2.source = node_by_name[target_str];
+      l2.target = node_by_name[source_str];
+      node_to_links[target_str].push(l2);
+    }
+  });
+
+  updateGraph();
+
+  $.isLoading("hide");
+});
